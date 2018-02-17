e2c464a8fb5faacd901e4a10fc91b76b18001e95
Rinkal <rdgandhi16@gmail.com>
2018-02-17 15:18:30 -0500

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "50000";
+            int numberconverted =int.Parse(number);
+
+            Console.WriteLine(numberconverted);
+            Console.ReadLine();
+        }
+
+    }
+}
